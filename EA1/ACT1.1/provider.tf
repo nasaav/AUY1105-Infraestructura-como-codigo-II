402b28fe98167e42767a8e9fafd1121fd06b0d67
MA-Alumno <MA-Alumno@duoc.acad>
2026-03-25 19:56:04 -0300

    Pequeño cambio sugerido

--- EA1/ACT1.1/provider.tf
+++ EA1/ACT1.1/provider.tf
@@ -1,3 +1,14 @@
+```bash
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
+```
